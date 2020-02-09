fe8900cca6fad5ff5efe6311b30594c01661b4e7
xhn <2996461110@qq.com>
2020-02-09 18:43:32 +0800

    update

--- Questions/141.cpp
+++ Questions/141.cpp
@@ -1,22 +1,32 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-#include <iostream>
+
+// Definition for singly-linked list.
+#include <set>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
 
-int main()
-{
-    return 0;
-    
-}
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode * cur;
+        cur  = head;
+
+        std::set<ListNode*> arr;
+
+        while(cur)
+        {
+            std::set<ListNode*>::iterator it = arr.find(cur);  
+            if(it != arr.end())
+                return true;
+            else
+                arr.insert(cur);
+
+            cur = cur->next;
+        }
+        return false;
+    }
+};
